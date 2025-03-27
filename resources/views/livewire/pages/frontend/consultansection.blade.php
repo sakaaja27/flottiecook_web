951b4810da4241bba2aefe0e98522e2ed85fa02f
Bramasta <bramastasaka9@gmail.com>
2025-03-27 10:22:41 +0700

    set up frontend publish and aibot

---
+++ resources/views/livewire/pages/frontend/consultansection.blade.php
@@ -0,0 +1,27 @@
+<section id="aibot" class="aibot section">
+    <div class="container section-title d-flex flex-column align-items-center" data-aos="fade-up">
+        <h1 class="font-bold text-black">AiBot <span class="text-pink-700">Recipes</span></h1>
+    </div>
+    <div class="container" data-aos="fade-up" data-aos-delay="100">
+        <div class="row gy-4 justify-content-between">
+            <div class="col-lg-4 order-lg-last hero-img" data-aos="zoom-out" data-aos-delay="100">
+                <dotlottie-player src="https://lottie.host/f26e29ce-f057-4cab-b609-a48962215c20/e1OVrsZonj.lottie"
+                    background="transparent" speed="1" style="width: 300px; height: 300px" loop
+                    autoplay></dotlottie-player>
+            </div>
+
+            <div class="col-lg-6  d-flex flex-column justify-content-center" data-aos="fade-in">
+                <h1>AiBot is ready to help find the food recipes sought</h1>
+                {{-- <p></p> --}}
+                <div class="d-flex mt-4">
+                    <button type="button"
+                        class="text-white bg-pink-700 hover:bg-pink-800 focus:outline-none focus:ring-4 focus:ring-pink-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800"><a
+                            href="./konsultasi" class="text-white">Started</a></button>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</section>
---
+++ resources/views/livewire/pages/frontend/sharerecipt.blade.php
@@ -0,0 +1,27 @@
+<section id="publish" class="publish section">
+    <div class="container section-title d-flex flex-column align-items-center" data-aos="fade-up">
+        <h1 class="font-bold text-black">Publish <span class="text-pink-700">Recipes</span></h1>
+    </div>
+    <div class="container" data-aos="fade-up" data-aos-delay="100">
+        <div class="row gy-4 justify-content-between">
+            <div class="col-lg-4 order-lg-last hero-img" data-aos="zoom-out" data-aos-delay="100">
+                <dotlottie-player src="https://lottie.host/866bcebe-c1e8-4920-8845-4faebe8539b1/nSVukLN1iG.lottie"
+                    background="transparent" speed="1" style="width: 300px; height: 300px" loop
+                    autoplay></dotlottie-player>
+            </div>
+
+            <div class="col-lg-6  d-flex flex-column justify-content-center" data-aos="fade-in">
+                <h1>You can share the food recipe you made, click start to try it and start sharing</h1>
+                {{-- <p></p> --}}
+                <div class="d-flex mt-4">
+                    <button type="button"
+                        class="text-white bg-pink-700 hover:bg-pink-800 focus:outline-none focus:ring-4 focus:ring-pink-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800"><a
+                            href="./konsultasi" class="text-white">Started</a></button>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</section>
